8ce3c50a54db28b279a70fe8537bbaeaac0c5aa5
balak26 <bamlakgezahegn26@gmail.com>
2024-08-07 12:45:33 +0300

    created a logic that creates rows and columns for grid that change colors when hovering

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
 const container = document.querySelector("#container");
 
-// function addGrid() {
-//   const newDiv = document.createElement("div");
-//   const newText = document.createTextNode("hi there");
-//   newDiv.appendChild(newText);
-//   container.appendChild(newDiv).classList.add("red");
-// }
-
-// addGrid();
-let n = 5;
-function addGridSquares() {
-  for (let i = 1; i <= n - 1; i++) {
+function addGridSquares(n) {
+  for (let i = 1; i <= n; i++) {
+    let column = document.createElement("div");
+    column.classList.add("cols");
     for (let j = 1; j <= n; j++) {
-      const newDiv = document.createElement("div");
-      newDiv.classList.add("cols");
-      container.appendChild(newDiv);
+      const row = document.createElement("div");
+      row.classList.add("row");
+      column.appendChild(row);
     }
+
+    container.appendChild(column);
   }
 }
 
-addGridSquares();
-
 container.addEventListener("mouseover", (e) => {
-  e.target.style.backgroundColor = "blue";
+  e.target.style.backgroundColor = "teal";
+});
+
+const btn = document.querySelector("#btn");
+
+function size() {
+  container.textContent = "";
+  let size = prompt("Enter the size of grid");
+  size = parseInt(size);
+  if (size > 100) {
+    alert("Size must me less than 100");
+  } else {
+    addGridSquares(size);
+  }
+}
+
+btn.addEventListener("click", () => {
+  size();
 });
